76841f1bd1e6df4d54b42a1a3b06f9cd705865d3
Jamile Yaacob <jamile.yaacob@student.rocva.nl>
2022-01-09 23:05:19 +0100

    2.6

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 
- $voornaam = "Tom";
- echo gettype($voornaam);
+$string = "string";
+$int = 3;
+$float = 3.5;
+$bool = false;
+$null = null;
 
- 
+echo gettype($string) . " " . gettype($int) . " " . gettype($float) . " " . gettype($bool) . " " . gettype($null);
